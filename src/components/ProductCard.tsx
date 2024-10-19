a034007181af895149167de2e05c805699e98587
ricardo <ricardodeveloper03@gmail.com>
2024-10-19 22:09:48 +0200

    extracting logic

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,9 +3,19 @@ import { deleteOneProductFromOwnRoom } from "../api/api";
 import { type ProductCardProps } from "../types/props";
 import ContentProductCard from "./ContentProductCard";
 import ContentEditableProductCard from "./ContentEditableProductCard";
+import { useUserStore } from "../store/userStore";
+import { Bounce, toast, ToastContainer } from "react-toastify";
+
+const toastStyle = {
+  pending: { backgroundColor: "#ffff8c", color: "black", fontWeight: "bold" },
+  success: { backgroundColor: "#8cff8c", color: "black", fontWeight: "bold" },
+  error: { backgroundColor: "#ff8c8c", color: "black", fontWeight: "bold" },
+};
 
 function ProductCard({ product }: ProductCardProps) {
   const [isEditing, setIsEditing] = useState(false);
+  const { access_token } = useUserStore((state) => state.user);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleClickToggleEditProduct = () => () => {
     setIsEditing(!isEditing);
@@ -18,11 +28,49 @@ function ProductCard({ product }: ProductCardProps) {
       roomUUID: `${string}-${string}-${string}-${string}-${string}`
     ) =>
     () => {
-      deleteOneProductFromOwnRoom(userId, productId, roomUUID);
+      setIsLoading(true);
+      toast
+        .promise(
+          deleteOneProductFromOwnRoom(
+            access_token,
+            userId,
+            productId,
+            roomUUID
+          ),
+          {
+            pending: {
+              render: "Eliminando producto...",
+              className: "pending-toast",
+              style: toastStyle.pending,
+            },
+            success: {
+              render: "Producto eliminado 🗑️",
+              className: "success-toast",
+              style: toastStyle.success,
+            },
+            error: {
+              render: "Algo salió mal 😱",
+              className: "error-toast",
+              style: toastStyle.error,
+            },
+          },
+          {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            theme: "dark",
+            transition: Bounce,
+          }
+        )
+        .finally(() => setIsLoading(false));
     };
 
   return (
     <>
+      <ToastContainer />
       {isEditing ? (
         <ContentEditableProductCard
           product={product}
@@ -33,6 +81,7 @@ function ProductCard({ product }: ProductCardProps) {
           handleClickToggleEditProduct={handleClickToggleEditProduct}
           product={product}
           handleClickDeleteProduct={handleClickDeleteProduct}
+          isLoading={isLoading}
         />
       )}
     </>
