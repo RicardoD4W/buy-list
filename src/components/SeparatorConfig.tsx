16e6e85bdd4452e931f77706274830200da27f13
ricardo <ricardodeveloper03@gmail.com>
2024-11-11 20:43:18 +0100

    separator styles

--- src/components/SeparatorConfig.tsx
+++ src/components/SeparatorConfig.tsx
@@ -5,8 +5,10 @@ function SeparatorConfig() {
 
   return (
     <hr
-      className="w-11/12 opacity-45"
-      style={{ borderColor: themeState.ContentColor }}
+      className="w-11/12 border-t border-dashed"
+      style={{
+        borderColor: themeState.ContentColor,
+      }}
     />
   );
 }
